b8d800179908b70688c518dec92e29f3a9dc2b79
Outrider99 <outrider.w@gmail.com>
2018-03-28 12:35:54 -0600

    Began artPanel

---
+++ src/art/view/ArtPanel.java
@@ -0,0 +1,17 @@
+package art.view;
+
+import java.awt.*;
+import javax.swing.*;
+import java.util.Hashtable;
+import javax.swing.event.*;
+import art.controller.ArtController;
+
+public class ArtPanel
+{
+	private final int MINIMUM_EDGE = 5;
+	private final int MAXIMUM_EDGE = 20;
+	private final int MINIMUM_SCALE = 20;
+	private final int MAXIMUM_SCALE = 100;
+	
+	private artController app;
+}
